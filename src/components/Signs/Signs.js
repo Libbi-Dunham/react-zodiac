55774d883f43b97a8f836de88c0791f5bff25a8c
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-11-30 16:14:39 -0800

    got the images and dates to pop up

--- src/components/Signs/Signs.js
+++ src/components/Signs/Signs.js
@@ -4,7 +4,7 @@ export default function signs({ name, dates }) {
   return (
     <>
       <div className="zodiac">
-        <img alt={name} src={`${process.env.PUBLIC_URL}/zodiacs/${name}.jpg`} />
+        <img alt={name} src={`${process.env.PUBLIC_URL}/${name}.png`} />
         <p className="name">{name}</p>
         <p>{dates}</p>
       </div>
