38f3f4b31b5798bfeabd03852608cb778d17367d
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-11-30 16:02:49 -0800

    trying to fix pathing

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Main.css'
-import '../Signs/Signs.js'
-import { zodiacs } from '../..data'
-import Zodiac from '../Signs/Signs.js'
-import background from '../..background.jpg'
+import { zodiac } from '../../data'
+import Zodiac from '../Signs/Signs'
+import background from '../../background.jpg'
 
 export default function Main() {
   return (
     <main className="background" style={{ backgroundImage: `url(${background})` }}>
-      {zodiacs.map((zodiac) => (
+      {zodiac.map((zodiac) => (
         <Zodiac key={zodiac.name} name={zodiac.name} dates={zodiac.dates} />
       ))}
     </main>
--- src/components/Signs/Signs.js
+++ src/components/Signs/Signs.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export default function signs() {
-  return <div></div>
+export default function signs({ name, dates }) {
+  return (
+    <>
+      <div className="zodiac">
+        <img alt={name} src={`${process.env.PUBLIC_URL}/zodiacs/${name}.jpg`} />
+        <p className="name">{name}</p>
+        <p>{dates}</p>
+      </div>
+    </>
+  )
 }
